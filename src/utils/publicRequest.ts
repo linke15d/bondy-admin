6e6e74985e537023f36d5a1161c0b2e506072089
link <linke15d@gmail.com>
2026-04-27 16:29:33 +0800

    fix

--- src/utils/publicRequest.ts
+++ src/utils/publicRequest.ts
@@ -2,6 +2,15 @@ import request from '@/config/axios'
 
 
 /**
+ * 用户登录
+ * @param data
+ * @returns
+ */
+export const loginOutApi = (data?): Promise<IResp> => {
+  return request.post({ url: '/api/userinfo/changepassword', data })
+}
+
+/**
  * 修改用户登录密码
  * @param data
  * @returns
